65e5ad3cda53a7450a67255a0aad1319b4c61041
nilberm <nilber16@outlookcom>
2024-02-17 14:02:57 -0300

    secont commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,59 @@ interface Product {
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [productsFiltered, setProductsFiltered] = useState<Product[]>([]);
+  const [textSearch, setTextSearch] = useState("");
 
   const getProducts = async () => {
     await api.get("products").then((response) => {
       console.log(response.data);
       setProducts(response.data);
+      setProductsFiltered(response.data);
     });
+  };
+
+  const handleSelectFilter = (value: string) => {
+    if (value === "none") {
+      setProductsFiltered(products);
+    }
+
+    if (value === "bestSellers") {
+      const sorted = [...products].sort(
+        (a, b) => b.rating.rate - a.rating.rate
+      );
+      setProductsFiltered(sorted);
+    }
+
+    if (value === "cheaper") {
+      const sorted = [...products].sort((a, b) => a.price - b.price);
+      setProductsFiltered(sorted);
+    }
+
+    if (value === "moreExpensive") {
+      const sorted = [...products].sort((a, b) => b.price - a.price);
+      setProductsFiltered(sorted);
+    }
+  };
+
+  const handleChangeSearch = (text: string) => {
+    setTextSearch(text);
+    // Verifica se o texto de pesquisa está vazio
+    if (text.trim() === "") {
+      // Se estiver vazio, define os produtos filtrados como a lista completa de produtos
+      setProductsFiltered(products);
+    } else {
+      // Caso contrário, filtra os produtos com base no texto de pesquisa
+      const filtered = products.filter(
+        (product) =>
+          // Aqui você pode ajustar as condições de filtro conforme necessário,
+          // como filtrar por título, descrição, categoria, etc.
+          product.title.toLowerCase().includes(text.toLowerCase()) ||
+          product.description.toLowerCase().includes(text.toLowerCase()) ||
+          product.category.toLowerCase().includes(text.toLowerCase())
+      );
+      // Define os produtos filtrados com base no resultado da filtragem
+      setProductsFiltered(filtered);
+    }
   };
 
   useEffect(() => {
@@ -37,10 +84,26 @@ export default function Home() {
       <h1>Fake Store</h1>
 
       <div className="content">
-        <div className="filters"></div>
+        <div className="filters">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(v) => handleChangeSearch(v.target.value)}
+          />
+          <select
+            name="filters"
+            id="filter"
+            onChange={(v) => handleSelectFilter(v.target.value)}
+          >
+            <option value="none">Ordenar Por</option>
+            <option value="bestSellers">Best Sellers</option>
+            <option value="cheaper">Price: Low to High</option>
+            <option value="moreExpensive">Price: High to Low</option>
+          </select>
+        </div>
         <div className="productsGrid">
-          {products &&
-            products.map((product: Product) => {
+          {productsFiltered.length > 0 ? (
+            productsFiltered.map((product: Product) => {
               return (
                 <div key={product.id} className="productItem">
                   <Image
@@ -58,12 +121,18 @@ export default function Home() {
                           <span className="rate">{product.rating.rate}</span>
                         </div>
                       </div>
+                      <button type="button" className="productPrice">
+                        $ {product.price}
+                      </button>
                       <div className="description">{product.description}</div>
                     </div>
                   </div>
                 </div>
               );
-            })}
+            })
+          ) : (
+            <div className="itemNotFound">No results for {textSearch} </div>
+          )}
         </div>
       </div>
     </MainContainer>
--- src/app/style.ts
+++ src/app/style.ts
@@ -6,16 +6,47 @@ export const MainContainer = styled.main`
   justify-content: center;
   align-items: center;
   padding: 2rem;
+  width: 100%;
 
   h1 {
     font-size: 2rem;
   }
 
   .content {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+
+    .filters {
+      display: flex;
+      gap: 1rem;
+      width: 100%;
+
+      input {
+        border: 3px solid #ddd;
+        border-radius: 5px;
+        width: 100%;
+        outline: none;
+        padding: 0.5rem;
+      }
+
+      select {
+        border: 3px solid #ddd;
+        border-radius: 5px;
+        padding: 0 0.5rem;
+      }
+    }
+
     .productsGrid {
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
       grid-gap: 1rem;
+
+      .itemNotFound {
+        display: flex;
+        width: 100%;
+      }
 
       .productItem {
         border: 1px solid #ccc;
@@ -41,7 +72,10 @@ export const MainContainer = styled.main`
             font-size: 1.2rem;
             font-weight: 600;
             height: 5rem;
-            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 6;
+            overflow: hidden;
           }
 
           .categoryAndRate {
@@ -49,11 +83,24 @@ export const MainContainer = styled.main`
             justify-content: space-between;
           }
 
+          .productPrice {
+            outline: none;
+            display: flex;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+            background: #22c55e;
+            font-weight: 600;
+            padding: 0.5rem;
+            border-radius: 5px;
+          }
+
           .description {
             display: -webkit-box;
             -webkit-box-orient: vertical;
             -webkit-line-clamp: 6;
             overflow: hidden;
+            max-width: 190px;
           }
         }
       }
